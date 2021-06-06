4b1efaf555101f319eaf4103b6cc272d65796d7b
Natallia Kryukova <>
2021-06-06 14:51:04 +0300

    init

---
+++ app/src/main/java/ru/startandroid/mintthursday/MainActivity.java
@@ -0,0 +1,44 @@
+package ru.startandroid.mintthursday;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
