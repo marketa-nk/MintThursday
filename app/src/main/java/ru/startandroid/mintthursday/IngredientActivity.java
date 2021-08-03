4ace0d1a07a53da0875bdc0cb64eced98ed4d891
Natallia Kryukova <krunat280@gmail.com>
2021-08-03 17:06:03 +0300

    IngredientActivity is made

--- app/src/main/java/ru/startandroid/mintthursday/IngredientActivity.java
+++ app/src/main/java/ru/startandroid/mintthursday/IngredientActivity.java
@@ -4,27 +4,96 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class IngredientActivity extends AppCompatActivity {
+
+    public static String INTENT_RESULT_ARG_NAME = "name";
+    public static String INTENT_RESULT_ARG_QUANTITY = "quantity";
+    public static String INTENT_RESULT_ARG_UNIT = "unit";
+    Button btnSave;
+    EditText ingrQty;
+    EditText ingrName;
+    MaterialAutoCompleteTextView ingrUnit;
+
+    private final TextWatcher ingrTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String ingrInput =ingrName.getText().toString().trim();
+            String qtyInput = ingrQty.getText().toString().trim();
+            String unitInput = ingrUnit.getText().toString().trim();
+            btnSave.setEnabled(!ingrInput.isEmpty() && !qtyInput.isEmpty() && !unitInput.isEmpty());
+        }
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient);
 
+        btnSave = findViewById(R.id.btnSave);
+        ingrQty = findViewById(R.id.textInputEditTextQty);
+        ingrName = findViewById(R.id.textInputEditTextName);
+        ingrUnit = findViewById(R.id.spinnerIngredUnits);
+
+        ingrName.addTextChangedListener(ingrTextWatcher);
+        ingrQty.addTextChangedListener(ingrTextWatcher);
+        ingrUnit.addTextChangedListener(ingrTextWatcher);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = String.valueOf(ingrName.getText());
+                String stringQuantity = String.valueOf(ingrQty.getText());
+                double quantity;
+                if (!stringQuantity.isEmpty()) {
+                    quantity = Double.parseDouble(stringQuantity);
+                } else {
+                    quantity = 0;
+                }
+                String unit = String.valueOf(ingrUnit.getText());
+
+                Intent intent = new Intent();
+                intent.putExtra(INTENT_RESULT_ARG_NAME, name);
+                intent.putExtra(INTENT_RESULT_ARG_QUANTITY, quantity);
+                intent.putExtra(INTENT_RESULT_ARG_UNIT, unit);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
+
         Toolbar ingredientToolbar = (Toolbar) findViewById(R.id.topAppBar);
         setSupportActionBar(ingredientToolbar);
         ingredientToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-
                 onBackPressed();
             }
         });
+
+        String[] type = getResources().getStringArray(R.array.ingredientUnits);
+        ingrUnit.setAdapter(new ArrayAdapter<>(this, R.layout.dropdown_menu_ingredient_unit, type));
 
     }
 
---
+++ app/src/main/java/ru/startandroid/mintthursday/IngredientAdapter.java
@@ -0,0 +1,102 @@
+package ru.startandroid.mintthursday;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.IngredientViewHolder> {
+    private List<Ingredient> ingredientList = new ArrayList<>();
+    OnItemClickListener ingredientListener;
+
+    public void addItem(Ingredient ingredient) {
+        ingredientList.add(ingredient);
+        notifyItemInserted(ingredientList.indexOf(ingredient));
+
+    }
+
+    public List<Ingredient> getItems() {
+        return ingredientList;
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public IngredientAdapter.IngredientViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.view_ingredient, parent, false);
+        return new IngredientViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(IngredientViewHolder holder, int position) {
+        holder.bind(ingredientList.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return ingredientList.size();
+    }
+
+    class IngredientViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView ingrName;
+        private TextView ingrQty;
+        private TextView ingrUnit;
+        private ImageButton btnIngrRemove;
+
+
+        public IngredientViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            ingrName = itemView.findViewById(R.id.ingr_name);
+            ingrQty = itemView.findViewById(R.id.ingr_qty);
+            ingrUnit = itemView.findViewById(R.id.ingr_unit);
+            btnIngrRemove = itemView.findViewById(R.id.ingr_remove);
+            btnIngrRemove.setOnClickListener(this);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Ingredient ingredient) {
+            ingrName.setText(ingredient.getName());
+            ingrQty.setText(String.valueOf(ingredient.getQuantity()));
+            ingrUnit.setText(ingredient.getUnit());
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (v.equals(btnIngrRemove)) {
+                deleteItem(getAdapterPosition());
+            } else if (ingredientListener != null) {
+                ingredientListener.onItemClick(v, getAdapterPosition());
+            }
+        }
+    }
+
+    private void deleteItem(int position) {
+        ingredientList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void setOnItemClickListener(final OnItemClickListener itemClickListener) {
+        this.ingredientListener = itemClickListener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+
+}
